ce5dd7277afd58119972301d7749646aaa943b54
Webblitchy <55515713+Webblitchy@users.noreply.github.com>
2022-10-14 10:27:55 +0200

    save name with button

--- app/src/main/java/ch/heig_vd/daa_lab01/FetchNameActivity.kt
+++ app/src/main/java/ch/heig_vd/daa_lab01/FetchNameActivity.kt
@@ -1,8 +1,11 @@
 package ch.heig_vd.daa_lab01
 
+import android.content.Intent
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.util.Log
 import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 
 class FetchNameActivity : AppCompatActivity() {
@@ -10,6 +13,16 @@ class FetchNameActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_save)
+
+        val buttonSave = findViewById<Button>(R.id.button_save)
+        val surnameInput = findViewById<EditText>(R.id.surname_input)
+
+        buttonSave.setOnClickListener {
+            val surname = surnameInput.text.toString();
+            // TODO: passer le surname en argument
+            val i = Intent(this, MainActivity::class.java)
+            startActivity(i)
+        }
     }
 
     companion object {
