0e76bde483fea7ea7534918a2660bc05af01a8c7
Luhrel <57545107+Luhrel@users.noreply.github.com>
2022-10-14 09:31:11 +0200

    add button action

--- app/src/main/java/ch/heig_vd/daa_lab01/MainActivity.kt
+++ app/src/main/java/ch/heig_vd/daa_lab01/MainActivity.kt
@@ -1,11 +1,20 @@
 package ch.heig_vd.daa_lab01
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttonEdit = findViewById<Button>(R.id.button_edit)
+
+        buttonEdit.setOnClickListener {
+            val i = Intent(this, FetchNameActivity::class.java)
+            startActivity(i)
+        }
     }
 }
