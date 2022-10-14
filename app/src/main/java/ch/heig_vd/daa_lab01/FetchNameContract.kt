07cb21e225822082275802fe0590331d709bfc53
Maxim Golay <m@xim.gy>
2022-10-14 09:34:34 +0200

    Implemented contract

--- app/src/main/java/ch/heig_vd/daa_lab01/FetchNameContract.kt
+++ app/src/main/java/ch/heig_vd/daa_lab01/FetchNameContract.kt
@@ -1,16 +1,21 @@
 package ch.heig_vd.daa_lab01
 
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import androidx.activity.result.contract.ActivityResultContract
 
-class FetchNameContract : ActivityResultContract<Void?, String>() {
+class FetchNameContract : ActivityResultContract<Void?, String?>() {
     override fun createIntent(context: Context, input: Void?): Intent =
-        Intent(context, )
+        Intent(context, FetchNameActivity::class.java)
 
 
-    override fun parseResult(resultCode: Int, intent: Intent?): String {
-        TODO("Not yet implemented")
+    override fun parseResult(resultCode: Int, intent: Intent?): String? {
+        // Check for status of returned contract
+        if (resultCode != Activity.RESULT_OK) return null
+
+        // If OK, return value stored at the agreed upon key
+        return intent?.getStringExtra(FetchNameActivity.ASK_FOR_NAME_RESULT_KEY)
     }
 
 }
