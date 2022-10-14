5a883f55790359815d406181a6ffeb2dba480023
Luhrel <57545107+Luhrel@users.noreply.github.com>
2022-10-14 09:27:05 +0200

    set content view

--- app/src/main/java/ch/heig_vd/daa_lab01/FetchNameActivity.kt
+++ app/src/main/java/ch/heig_vd/daa_lab01/FetchNameActivity.kt
@@ -2,11 +2,13 @@ package ch.heig_vd.daa_lab01
 
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class FetchNameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        TODO("Something")
+
+        setContentView(R.layout.activity_save)
     }
 }
