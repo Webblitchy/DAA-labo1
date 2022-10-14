4bbf60a9ca41a2bae595695a1b18740e4b091308
Maxim Golay <m@xim.gy>
2022-10-14 09:21:24 +0200

    Implemented skeleton of FetchName stuff

---
+++ app/src/main/java/ch/heig_vd/daa_lab01/FetchNameActivity.kt
@@ -0,0 +1,12 @@
+package ch.heig_vd.daa_lab01
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class FetchNameActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        TODO("Something")
+    }
+}
--- app/src/main/java/ch/heig_vd/daa_lab01/FetchNameContract.kt
+++ app/src/main/java/ch/heig_vd/daa_lab01/FetchNameContract.kt
@@ -1,4 +1,16 @@
 package ch.heig_vd.daa_lab01
 
-class FetchNameContract {
+import android.content.Context
+import android.content.Intent
+import androidx.activity.result.contract.ActivityResultContract
+
+class FetchNameContract : ActivityResultContract<Void?, String>() {
+    override fun createIntent(context: Context, input: Void?): Intent =
+        Intent(context, )
+
+
+    override fun parseResult(resultCode: Int, intent: Intent?): String {
+        TODO("Not yet implemented")
+    }
+
 }
