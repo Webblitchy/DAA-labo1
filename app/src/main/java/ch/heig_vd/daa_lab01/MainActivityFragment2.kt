5a3bf6b3390e51c3307f7b1b59f44792aa542b1b
Maxim Golay <m@xim.gy>
2022-10-21 09:21:05 +0200

    Added activity for last exo

---
+++ app/src/main/java/ch/heig_vd/daa_lab01/MainActivityFragment2.kt
@@ -0,0 +1,12 @@
+package ch.heig_vd.daa_lab01
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivityFragment2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fragment2)
+    }
+}
