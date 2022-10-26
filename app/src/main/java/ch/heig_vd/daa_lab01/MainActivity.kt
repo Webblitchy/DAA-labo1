d8a254cf658528236fe8c6cb4413f6ba01ec2dfc
Luhrel <57545107+Luhrel@users.noreply.github.com>
2022-10-26 22:54:37 +0200

    Fix typo

--- app/src/main/java/ch/heig_vd/daa_lab01/MainActivity.kt
+++ app/src/main/java/ch/heig_vd/daa_lab01/MainActivity.kt
@@ -10,7 +10,7 @@ class MainActivity : AppCompatActivity() {
 
     private val getName = registerForActivityResult(FetchNameContract()) {
         name = (
-            if (it.isNullOrBlank()) "Bievenue, veuillez entrer votre nom"
+            if (it.isNullOrBlank()) "Bienvenue, veuillez entrer votre nom"
             else "Bienvenue ${it} !"
         )
 
