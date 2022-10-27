d000dac03967e4275d8f40a7144220671b3932da
Luhrel <57545107+Luhrel@users.noreply.github.com>
2022-10-27 15:32:42 +0200

    Step on the first fragment

--- app/src/main/java/ch/heig_vd/daa_lab01/MainActivityFragment2.kt
+++ app/src/main/java/ch/heig_vd/daa_lab01/MainActivityFragment2.kt
@@ -7,6 +7,8 @@ import androidx.appcompat.app.AppCompatActivity
 import ch.heig_vd.daa_lab01.fragments.StepFragment
 
 class MainActivityFragment2 : AppCompatActivity() {
+    var first_time = true
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_fragment2)
@@ -29,11 +31,23 @@ class MainActivityFragment2 : AppCompatActivity() {
             supportFragmentManager.beginTransaction()
                 .replace(
                     R.id.fragmentContainer,
-                    StepFragment.newInstance(supportFragmentManager.backStackEntryCount)
+                    StepFragment.newInstance(supportFragmentManager.backStackEntryCount + 1)
                 )
                 .addToBackStack(null)
                 .commit()
-
         }
+
+        savedInstanceState?.let {
+            first_time = it.getBoolean("first_time")
+        }
+        if (first_time) {
+            findViewById<Button>(R.id.button_next).callOnClick()
+            first_time = false
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putBoolean("first_time", first_time)
     }
 }
